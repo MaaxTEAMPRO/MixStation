df6cab173cda5dd5dfd81d141d2ed3d5add8cd56
MaaxTEAMPRO <22edirlei@gmail.com>
2024-06-18 02:26:32 -0300

    aaaaa

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ async function fetchStations() {
         { name: 'Band FM Floripa', url: 'https://cast2.youngtech.radio.br/radio/8010/radio', icon: 'https://pubimg.band.uol.com.br/Files/bandfm-live.png' },
   
         { name: 'Italia anni 60', url: 'https://str01.fluidstream.net/anni60.mp3', icon: 'https://www.radioitaliaannisessanta.it/images/ria60/za_network/inserzioni/radioitaliaanni60.jpg' },
-        { name: '181 FM - Country 80s', url: 'http://listen.livestreamingservice.com/181-80scountry_128k.mp3', icon: 'https://m.media-amazon.com/images/I/518USI0buaL.png' },
+        { name: '100.9 Classic Country', url: 'https://live.amperwave.net/direct/wboc-waaifmmp3-ibc2?_=84', icon: 'https://mm.aiircdn.com/609/5cd2c3c93418e.png' },
         { name: '181 FM - Country 90s', url: 'http://listen.livestreamingservice.com/181-90scountry_128k.mp3', icon: 'https://' },
         { name: '181 FM - Classic Hits 181', url: 'http://listen.livestreamingservice.com/181-greatoldies_128k.mp3', icon: 'https://m.media-amazon.com/images/I/518USI0buaL.png' },
         { name: '181 FM - Goodtime 60s', url: 'http://listen.livestreamingservice.com/181-goodtime_128k.mp3', icon: 'https://m.media-amazon.com/images/I/518USI0buaL.png' },
